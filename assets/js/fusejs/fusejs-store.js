c65cd064a101346856b2e049dc321e6c496e9346
samc5 <75958455+samc5@users.noreply.github.com>
2025-09-22 08:45:34 +0000

    Deploying to gh-pages from @ bu-sports-analytics/bu-sports-analytics.github.io@c0ac661f33c3f8b465789afb30bda5692ba81cfe 🚀

--- assets/js/fusejs/fusejs-store.js
+++ assets/js/fusejs/fusejs-store.js
@@ -14,56 +14,6 @@ var store = [{
         "url": "https://bu-sports-analytics.github.io/archive",
         "img": null
       },{
-        "title": "BUSAG Newsletter Issue 1",
-        "subtitle": "",
-        "excerpt": "<p>Welcome to Boston University’s First</p>",
-        "content": "&lt;center&gt; Welcome to Boston University’s First Sports Analytics Group! Thank you for joining us at our first meeting! If you couldn’t attend, no worries—stay tuned for event updates on Instagram....",
-        "categories": [],
-        "date": "2025-02-02",
-        "tags": [],
-        "url": "https://bu-sports-analytics.github.io/20250202_newsletter1/",
-        "img": null
-      },{
-        "title": "BUSAG Newsletter Issue 2",
-        "subtitle": "",
-        "excerpt": "<p>BUSAG Announcements</p>",
-        "content": "&lt;center&gt; BUSAG Announcements Hey everyone! Hope you all are enjoying the long weekend. We wanted to let you guys know that our second meeting is officially scheduled for Tuesday February...",
-        "categories": [],
-        "date": "2025-02-15",
-        "tags": [],
-        "url": "https://bu-sports-analytics.github.io/20250215_newsletter2/",
-        "img": null
-      },{
-        "title": "BUSAG Newsletter Vol 3",
-        "subtitle": "",
-        "excerpt": "<p>Club Updates and General Meetings</p>",
-        "content": "&lt;center&gt; Club Updates and General Meetings Hello everyone! Our upcoming meeting is scheduled for 6:00 on Monday March 3rd, with the room information to be announced. Keep an eye out...",
-        "categories": [],
-        "date": "2025-02-28",
-        "tags": [],
-        "url": "https://bu-sports-analytics.github.io/20250228_newsletter3/",
-        "img": null
-      },{
-        "title": "BUSAG Newsletter Vol 4",
-        "subtitle": "",
-        "excerpt": "<p>Updates for the Coming Weeks</p>",
-        "content": "&lt;center&gt; Updates for the Coming Weeks Hello everyone! As we begin to wrap up the semester, we wanted to share the dates for our final few meetings/events of the year:...",
-        "categories": [],
-        "date": "2025-03-04",
-        "tags": [],
-        "url": "https://bu-sports-analytics.github.io/20250304_newsletter5/",
-        "img": null
-      },{
-        "title": "Submit Your March Madness Bracket Challenge Now🏀",
-        "subtitle": "",
-        "excerpt": "<p>March Madness Brackets!</p>",
-        "content": "&lt;center&gt; March Madness Brackets! Hi everyone! For those of you unable to attend our bracket making event, we are sharing the link so that you are still able to participate!...",
-        "categories": [],
-        "date": "2025-03-18",
-        "tags": [],
-        "url": "https://bu-sports-analytics.github.io/20250318_newsletter4/",
-        "img": null
-      },{
         "title": "Congrats to Tejas and Sam on winning the 2025 Ohio State Sports Analytics Hackathon!",
         "subtitle": "",
         "excerpt": "<p>See the winning presentation attached</p>",
@@ -89,7 +39,7 @@ var store = [{
         "excerpt": "<p>A demo page checking Markdown</p>",
         "content": "What’s Markdown? Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents. Created by John Gruber in 2004, Markdown is now one...",
         "categories": ["tutorial"],
-        "date": "2025-08-24",
+        "date": "2025-09-22",
         "tags": ["starter","syntax","markdown"],
         "url": "https://bu-sports-analytics.github.io/cheatsheets/01-markdown-cheat-sheet",
         "img": null
@@ -99,7 +49,7 @@ var store = [{
         "excerpt": "<p>kramdown supercharges Markdown with some interesting features.</p>",
         "content": "What’s kramdown? kramdown supercharges Markdown with some interesting features. kramdown is the default Jekyll Markdown processor. When creating your site with Jekyll, you can use the standard Markdown syntax plus...",
         "categories": ["tutorial"],
-        "date": "2025-08-24",
+        "date": "2025-09-22",
         "tags": ["starter","syntax","kramdown"],
         "url": "https://bu-sports-analytics.github.io/cheatsheets/02-kramdown-cheat-sheet",
         "img": null
